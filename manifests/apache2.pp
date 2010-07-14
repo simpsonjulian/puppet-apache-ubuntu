379755ff1dcf618186506de1c6aa1b387a5959f8
simpsonjulian <simpsonjulian@gmail.com>
2010-07-14 22:54:58 +0100

    roll back silly changes

--- manifests/apache2.pp
+++ manifests/apache2.pp
@@ -1,18 +1,17 @@
 class apache2 {
-$www_doc_dir = '/data/www/doc'
-$www_log_dir = '/data/www/log'
-$www_rails_root = '/data/www/rails'
 
   class install { 
+    include apache2
+
     file { 
       "/data": ensure => directory;
       "/data/www": ensure => directory, require => File['/data'];
-      $apache2::www_doc_dir: 
+      $www_doc_dir: 
         owner => www-data,
         ensure => directory, 
         require => File['/data/www'];
-      $apache2::www_log_dir: ensure => directory, require => File["/data/www"];  
-      $apache2::www_rails_root: ensure => directory, require => File["/data/www"];  
+      $www_log_dir: ensure => directory, require => File["/data/www"];  
+      $www_rails_root: ensure => directory, require => File["/data/www"];  
 
       "/etc/logrotate.d/apache2":
         owner => root,
